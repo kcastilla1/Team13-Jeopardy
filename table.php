5ab676cacb83cb043644b8421be229f5d4dc3e0b
nhitran195 <93238994+nhitran195@users.noreply.github.com>
2022-03-23 07:41:00 -0400

    Add files via upload

---
+++ table.php
@@ -0,0 +1,64 @@
+<html>
+
+<!-- WebPro Spring 2022 -->
+<!-- Author: Kendall Castilla -->
+<!-- Date: 03/23/2022 -->
+
+<head>
+    <link href="style.css" type="text/css" rel="stylesheet">
+    <style>
+        
+    </style>
+    <title>Jeopardy!</title>
+</head>
+
+<body>
+<h1 class = "title">Jeopardy!</h1>
+    <div class = "table">
+        <table>
+            <tr>
+                <!--Col 1--><th>State, Not Southern</th>
+                <!--Col 2--><th>Palindromes</th>
+                <!--Col 3--><th>Anagrams</th>
+                <!--Col 4--><th>Column 4</th>
+                <!--Col 5--><th>Column 5</th>
+                <!--Col 6--><th>Column 6</th>
+            </tr>
+            <tr>
+                <?php for($i=0; $i<6; $i++) {
+                    echo "<td>$100</td>";
+                }
+                ?>
+                
+            </tr>
+            <tr>
+                <?php for($i=0; $i<6; $i++) {
+                    echo "<td>$200</td>";
+                }
+                ?>
+            </tr>
+            <tr>
+                <?php for($i=0; $i<6; $i++) {
+                    echo "<td>$300</td>";
+                }
+                ?>
+            </tr>
+            <tr>
+                <?php for($i=0; $i<6; $i++) {
+                    echo "<td>$400</td>";
+                }
+                ?>
+            </tr>
+            <tr>
+                <?php for($i=0; $i<6; $i++) {
+                    echo "<td>$500</td>";
+                }
+                ?>
+            </tr>
+        </table>
+    </div>
+
+</body>
+
+
+</html>
