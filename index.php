278f286d2791619dbd975269d55a751eb1517893
nhitran195 <93238994+nhitran195@users.noreply.github.com>
2022-03-23 06:52:20 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Jeopardy!</title>
+<link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <br>
+    <div class="intro">JEOPARDY!</div>
+    <br>
+       <div class = "userinfo">
+          <form action = "signup.php" method = "post">
+        <label> Your name: </label> <input name= "name" type = "text" size = "25">
+        <br>
+        <br>
+        <input class = "button" type = "submit" value = "Play game!">
+      </form>
+    </div>
+     
+  </body>
+</html>
